31585942716a95c319aac718cb5b225a6734e37b
Rishi J <rishij@Rishis-MacBook-Air.local>
2024-12-29 22:06:10 +0530

    Add Bonus Extra Regex Matching Cases ideally along with Rubocop Fixes

--- calculator.rb
+++ calculator.rb
@@ -5,17 +5,32 @@ class Calculator
 
     # Using Regex matching to split either based on comma or new line
     delimiter = /,|\n/ # Default Delimiter
-    if numbers.start_with?("//")
-      custom_delimiter, numbers = numbers[2..].split("\n", 2)
-      delimiter = custom_delimiter
+    if numbers.start_with?('//')
+      delimiter_section, numbers = numbers[2..].split("\n", 2)
+      delimiter = parse_delimiters(delimiter_section)
     end
 
     # Using Map function and .to_i to change it to a integer list and perfom summation on the same
     num_list = numbers.split(delimiter).map(&:to_i)
+    puts(delimiter)
     negatives = num_list.select(&:negative?)
 
     raise "Negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
 
     num_list.sum
+  end
+
+  private
+
+  def parse_delimiters(delimiter_section)
+    if delimiter_section.start_with?("[") && delimiter_section.end_with?("]")
+      # Extract each delimiter enclosed in square brackets
+      delimiters = delimiter_section.scan(/\[([^\]]+)\]/).flatten
+      # Escape special characters and combine into a single regex
+      Regexp.new(delimiters.map { |d| Regexp.escape(d) }.join('|'))
+    else
+      # Single character delimiter
+      Regexp.new(Regexp.escape(delimiter_section))
+    end
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -5,22 +5,22 @@ RSpec.describe Calculator do
   describe '#add' do
     it 'returns 0 for an empty string' do
       calculator = Calculator.new
-      expect(calculator.add("")).to eq(0)
+      expect(calculator.add('')).to eq(0)
     end
 
     it 'returns the number itself for a single number string' do
       calculator = Calculator.new
-      expect(calculator.add("1")).to eq(1)
+      expect(calculator.add('1')).to eq(1)
     end
 
     it 'returns the sum of two numbers' do
       calculator = Calculator.new
-      expect(calculator.add("1,2")).to eq(3)
+      expect(calculator.add('1,2')).to eq(3)
     end
 
     it 'returns the sum of two numbers' do
       calculator = Calculator.new
-      expect(calculator.add("1,2,10,20")).to eq(33)
+      expect(calculator.add('1,2,10,20')).to eq(33)
     end
 
     it 'handles new lines and commas between numbers' do
@@ -35,7 +35,22 @@ RSpec.describe Calculator do
 
     it 'raises an exception for negative numbers' do
       calculator = Calculator.new
-      expect { calculator.add("1,-2,3,-4") }.to raise_error("Negative numbers not allowed: -2, -4")
+      expect { calculator.add('1,-2,3,-4') }.to raise_error('Negative numbers not allowed: -2, -4')
+    end
+
+    it 'supports delimiters of any length' do
+      calculator = Calculator.new
+      expect(calculator.add("//[***]\n1***2***3")).to eq(6)
+    end
+
+    it 'supports multiple delimiters' do
+      calculator = Calculator.new
+      expect(calculator.add("//[*][%]\n1%5*3")).to eq(9)
+    end
+
+    it 'supports multiple delimiters with length longer than one character' do
+      calculator = Calculator.new
+      expect(calculator.add("//[***][%%]\n1***2%%3")).to eq(6)
     end
   end
 end
